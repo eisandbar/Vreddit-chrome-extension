75113f486a20fa0c549bfc6bd228839712320916
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-11-30 23:50:43 +0300

    Added context menu which downloads the video when clicked

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-var videoOptions = {}
-var origin
+var videoOptions = {} // The data needed to download the video
+var origin // Making sure that we are downloading from the same page where the video is located
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.url !== undefined && request.url !== "") {
@@ -9,5 +9,21 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             filename: request.file ? request.file : "video.mp4",
           }
         origin = request.origin
+    }
+})
+
+var contextMenuItem = {
+    "id": "videoDownload",
+    "title": "Download vreddit video",
+    "contexts": ["all"] // Can't use video as reddit videos hide behind a div == aren't clickable
+}
+
+chrome.contextMenus.create(contextMenuItem)
+
+chrome.contextMenus.onClicked.addListener(function (OnClickData) {
+    if (videoOptions.url !== undefined && videoOptions.url !== "" && OnClickData.pageUrl == origin) {
+        chrome.downloads.download(
+            options=videoOptions
+        )
     }
 })
