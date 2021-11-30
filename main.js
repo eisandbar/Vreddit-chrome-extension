357e7fd9223f43ea3bde85fcc980594e78c13007
eisandbar <55670245+eisandbar@users.noreply.github.com>
2021-11-30 23:43:41 +0300

    main.js now checks the page json data to see if there is a video on the page

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ let lastUrl = window.location.href
 onUrlChange(lastUrl)
 
 new MutationObserver(() => {
-  const url = window.location.href
-  if (url !== lastUrl) {
-    lastUrl = url
-    onUrlChange(url)
-  }
+    // This observer will check for changes in the document and call function onUrlChange when the page url changes
+    const url = window.location.href
+    if (url !== lastUrl) {
+        lastUrl = url
+        onUrlChange(url)
+    }
 }).observe(document, {subtree: true, childList: true})
 
-function onUrlChange(url) { 
+function onUrlChange(url) {
+    // Creating a GET request to see if the reddit page contains a video
+    const req = new XMLHttpRequest()
+    req.responseType = 'json';
+    req.open("GET", url+'.json')
+    req.send()
+    req.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log("Got page json data")
+            try {
+            const video = this.response[0].data.children[0].data.secure_media.reddit_video.fallback_url
+            console.log(video)
+            } catch {
+            console.log("No videos here")
+            }
+            
+        }
+    }
 }
